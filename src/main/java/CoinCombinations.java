fb7cd5b6dae24dc83685ee3ec6d043de78812b11
Joshua Gustafson and Kyle Derrick <silversevenfox@yahoo.com>
2016-02-11 14:18:17 -0800

    add UI plus UI styles and 2 integration tests

--- src/main/java/CoinCombinations.java
+++ src/main/java/CoinCombinations.java
@@ -6,7 +6,36 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class CoinCombinations {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+    get("/", (request, response) -> {
+      HashMap model = new HashMap();
+
+    model.put("template", "templates/index.vtl");
+    return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+    get("/results", (request, response) -> {
+      HashMap model = new HashMap();
+      String userNumber = request.queryParams("numberInput");
+      Integer intUserNumber = Integer.parseInt(userNumber);
+      Integer[] userCoinArray = coinCounter(intUserNumber);
+      Integer pennies = userCoinArray[0];
+      Integer nickels = userCoinArray[1];
+      Integer dimes = userCoinArray[2];
+      Integer quarters = userCoinArray[3];
+
+      model.put("pennies", pennies);
+      model.put("nickels", nickels);
+      model.put("dimes", dimes);
+      model.put("quarters", quarters);
+
+      model.put("template", "templates/results.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
 
 public static Integer[] coinCounter(Integer userNumber) {
   Integer[] coinArray = {0, 0, 0, 0};
